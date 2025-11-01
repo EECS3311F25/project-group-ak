f0fc4e0190ba1e5b5ce564edc49b74419286ac9a
Ciftelia <klodiana.kamberi87@gmail.com>
2025-11-01 19:30:56 -0400

    [Sprint 1] Clean up merge with devClean.

--- KotlinProject/composeApp/src/commonMain/kotlin/org/example/project/model/Colors.kt
+++ KotlinProject/composeApp/src/commonMain/kotlin/org/example/project/model/Colors.kt
@@ -1,8 +1,8 @@
-package com.plcoding.bookpedia.core.presentation
+package org.example.project.model
 
 import androidx.compose.ui.graphics.Color
 
-val DarkBlue = Color(0xFF0B405E)
-val DesertWhite = Color(0xFFF7F7F7)
-val SandYellow = Color(0xFFFFBD64)
-val LightBlue = Color(0xFF9AD9FF)
+val PRIMARY = Color(0xFF0B405E)
+val BACKGROUND = Color(0xFFF7F7F7)
+val SECONDARY = Color(0xFFFFBD64)
+val TERTIARY = Color(0xFF9AD9FF)
--- KotlinProject/composeApp/src/commonMain/kotlin/org/example/project/view/Colours.kt
+++
@@ -1,5 +0,0 @@
-package org.example.project.view
-
-import androidx.compose.ui.graphics.Color
-
-val mainGreen = Color(red = 0.5f, green = 1f, blue = 0.6f)
