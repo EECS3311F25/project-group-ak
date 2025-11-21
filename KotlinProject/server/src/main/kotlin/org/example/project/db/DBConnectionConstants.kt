d4993390a6b66dc7ada46e777bbf0879ade762fe
Fish <116611743+fishonttree@users.noreply.github.com>
2025-11-20 19:51:52 -0500

    [SCRUM-64] Added .yml file for PostgreSQL dockerization

--- KotlinProject/server/src/main/kotlin/org/example/project/db/DBConnectionConstants.kt
+++ KotlinProject/server/src/main/kotlin/org/example/project/db/DBConnectionConstants.kt
@@ -2,8 +2,8 @@ package org.example.project.db
 
 object DBConnectionConstants {
     const val POSTGRES_DB_HOST = "localhost"
-    const val POSTGRES_DB_PORT = 5433
+    const val POSTGRES_DB_PORT = 5432
     const val POSTGRES_DB_NAME = "navi_db"
     const val POSTGRES_USER_NAME = "postgres"
-    const val POSTGRES_USER_PSW = "kfegw!9ATFON"
+    const val POSTGRES_USER_PSW = "postgres"
 }
